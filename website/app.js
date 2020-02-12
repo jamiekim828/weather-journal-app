4ce8e38aba431f1faae76554473763c4edeb7b46
jamiekim828 <jamiekim828@gmail.com>
2020-02-12 15:26:47 +0100

    GET API data function added

--- website/app.js
+++ website/app.js
@@ -7,11 +7,28 @@ let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
 // Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
 
 /* Function called by event listener */
+function performAction(e) {
+  const newZip = document.getElementById('zip').value;
+  const newFeeling = document.getElementById('feelings').value;
+  getWeather(baseUrl, newZip, apiKey);
+}
 
 /* Function to GET Web API Data*/
+const getWeather = async (baseUrl, zip, apiKey) => {
+  const res = await fetch(baseUrl + zip + '&appid=' + apiKey);
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
 
 /* Function to POST data */
+const postWeather = async (url = '/adddata', data = {}) => {};
 
 /* Function to GET Project Data */
